077cf5a15881d2ad40ebc5eda5aefb71ce909529
Willian Mano <willian.mano@jove.com>
2023-05-23 16:01:20 -0300

    Finaliza a adicao de carga horaria nas atividades

--- lang/en/block_progress_time.php
+++ lang/en/block_progress_time.php
@@ -5,5 +5,6 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Bloco Progresso UEMS';
 $string['progress_time:addinstance'] = 'Permissão de adicionar o bloco no curso';
 $string['progress_time:viewreport'] = 'Permissão de ver o relatório';
-$string['progresstimeformheader'] = 'Configurações de progresso';
+$string['progresstimeformheader'] = 'Configurações de carga horária';
 $string['activityhours'] = 'Horas da atividade';
+$string['activityhours_help'] = 'Este campo permite configurar a carga horária desta atividade.';
--- lib.php
+++ lib.php
@@ -12,6 +12,8 @@ defined('MOODLE_INTERNAL') || die();
  * (the mod is assumed for course activities)
  */
 function block_progress_time_coursemodule_standard_elements($formwrapper, $mform) {
+    global $DB;
+
     if (!block_progress_time_course_has_block($formwrapper->get_current()->course)) {
         return;
     }
@@ -22,17 +24,62 @@ function block_progress_time_coursemodule_standard_elements($formwrapper, $mform
         45 => 45, 50 => 50, 60 => 60, 70 => 70, 80 => 80, 90 => 90, 100 => 100);
 
     $mform->addElement('select', 'activityhours', get_string('activityhours', 'block_progress_time'), $options);
+    $mform->addHelpButton('activityhours', 'activityhours', 'block_progress_time');
     $mform->setType('activityhours', PARAM_INT);
+    // Se a conclusao de atividade estiver desabilitado, desabilitar o campo.
+    $mform->disabledIf('activityhours', 'completion', 'eq', 0);
 
     if (!is_null($formwrapper->get_coursemodule()) && $cmid = $formwrapper->get_coursemodule()->id) {
-        $cmid = $cmid;
+        if ($activityhours = $DB->get_record('progress_time_modules', ['cmid' => $cmid], 'id, value')) {
+            $mform->setDefault('activityhours', $activityhours->value);
+        }
     }
 }
 
 function block_progress_time_coursemodule_edit_post_actions($moduleinfo, $course) {
+    if (isset($moduleinfo->activityhours)) {
+        block_progress_time_sync_activityhours($moduleinfo->coursemodule, (int) $moduleinfo->activityhours);
+    }
+
     return $moduleinfo;
 }
 
-function block_progress_time_course_has_block($courseid) {
+function block_progress_time_sync_activityhours($cmid, $activityhours) {
+    global $DB;
+
+    $data = $DB->get_record('progress_time_modules', ['cmid' => $cmid]);
+
+    // Atualiza valor no banco.
+    if ($data && $activityhours > 0) {
+        $data->value = $activityhours;
+        $data->timemodified = time();
+
+        return $DB->update_record('progress_time_modules', $data);
+    }
+
+    // Insere novo registro no banco.
+    if (!$data && $activityhours > 0) {
+        $data = new \stdClass();
+        $data->value = $activityhours;
+        $data->cmid = $cmid;
+        $data->timecreated = time();
+        $data->timemodified = time();
+
+        return $DB->insert_record('progress_time_modules', $data);
+    }
+
+    // Exclui registro do banco.
+    if ($data && $activityhours == 0) {
+        return $DB->delete_records('progress_time_modules', ['cmid' => $cmid]);
+    }
+
     return true;
+}
+
+function block_progress_time_course_has_block($courseid) {
+    global $DB;
+
+    $context = \context_course::instance($courseid);
+
+    return $DB->record_exists('block_instances', ['blockname' => 'progress_time', 'parentcontextid' => $context->id]);
 }
